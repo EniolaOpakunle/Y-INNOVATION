24065f257c7b9ee720e670612213a8e5549c01d4
Adegoke julius <adegokejulius46@gmail.com>
2023-09-09 22:28:39 +0100

    yh

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -15,7 +15,7 @@ function Main({handle}) {
             <div className='col-lg-5  col-md-9 py-4'>
               <h3 className='text1'>{t('home.1')}<span className='text-light'>{t('home.2')} !!!</span></h3>
               <h1 className='text2 mt-3'>{t('home.3')} <span className=''>{t('home.4')}</span>{t('home.5')}</h1>
-              <p className=' my-4 text-light text'>Y INNOVATIONS {t('home.6')}</p>
+              <p className=' my-4 text-light text'> {t('home.6')}</p>
               <div className='mt-4 d-flex'>
                 <a className='btn px-2 btn1 title text-light ' href = "https://t.me/Yinnovations">{t('home.7')} <img src={joinImg} alt=""/></a>
                 <a className='btn px-2 button1 mx-3 text-light title'  href='https://poocoin.app/tokens/0xc87138284116dc1b33c1cb2496beb3df73a53ee2' >{t('home.8')} <img src={buyImg} alt="" /></a>
